906e6e03bf09820d5871af6aa2655b513fc3f8b6
Juhász Viktória 13c haladó <116707126+JuhaszViktoriaa@users.noreply.github.com>
2025-09-09 10:38:41 +0200

    Add files via upload
    event duration calculator project

---
+++ ismetles/EventDurationCalculatorProject/EventDurationCalculator.cs
@@ -0,0 +1,20 @@
+﻿namespace EventDurationCalculatorProject
+{
+    internal static class EventDurationCalculator
+    {
+        public static (DateOnly, int, int) CalculateEnd(DateOnly startDate, int startHour, int startMinute, int durationHours, int durationMinutes)
+        {
+            int osszesperc = startMinute + durationMinutes;
+            int vegperc = osszesperc % 60;                  //1 ora 15 perc plussz
+            int extraora = osszesperc / 60;
+
+            int teljesido = startHour + durationHours + extraora;
+            int vegora = teljesido % 24;
+            int extranap = teljesido / 24;
+
+            DateOnly veglegesadat=startDate.AddDays(extranap);
+
+            return (veglegesadat, vegora, vegperc);
+        }
+    }
+}
