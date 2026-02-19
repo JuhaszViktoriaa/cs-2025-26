e22189161b241665a200da643b2d0f96b1e0ecaf
Juhász Viktória 13c <116707126+JuhaszViktoriaa@users.noreply.github.com>
2026-02-19 10:30:22 +0100

    Add files via upload
    desktop scaffolding

---
+++ Repos/RestaurantsRepo.cs
@@ -0,0 +1,30 @@
+﻿using MyExam.Desktop.DbSqliteModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExam.Desktop.Repos
+{
+    public class RestaurantsRepo    //backend, linq desktop
+    {
+        private readonly RestaurantsContext _context = new RestaurantsContext();
+
+        public int Count => _context.Restaurants.Count();
+
+        public int RevenueCount => _context.Restaurants.Count(r => r.Income > 25000);
+
+        public int RevenueLess => _context.Restaurants.Count(r => r.Income < 20000);
+
+        public int NagyAnna => _context.Restaurants.Where(o => o.Owner == "Nagy Anna").Max(r => r.Income);
+
+        //felkesz
+        //public int KissGabor => _context.Restaurants.GroupBy(o => o.Owner == "Nagy Anna").Select(g => new { owner = g.Key, bevetel = Max.(g.Income) });
+
+        public int BetweenRevenue => _context.Restaurants.Count(r => r.Income > 20000 && r.Income < 40000);
+
+        //felkesz
+        //public int SumSzaboLaszlo => _context.Restaurants.Sum(r => r.Income).Where(o => o.Owner == "Szabó László");
+    }
+}
